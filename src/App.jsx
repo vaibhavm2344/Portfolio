5c895b63831cd79d2354e3503c264e43de872763
Vaibhav More <vaibhavcmore23@gmail.com>
2025-08-28 00:54:35 +0530

    finish initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import Home from "./components/Home";
-import AboutMe from "./components/AboutMe";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import AboutMe from "./pages/AboutMe";
+import Contact from "./pages/Contact";
+import Skills from "./pages/Skills";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <div>
-        <Home />
-        <AboutMe />
-        <Projects />
-        <Skills />
-        <Contact />
+    <div >
+      <div className="hidden bg-black max-sm:flex-col min-h-screen max-sm:flex justify-center px-6 gap-3">
+        <h1 className="text-white text-2xl font-semibold text-center">Go to desktop.</h1>
+        <p className="text-white text-2xl text-center">I didn't have time for mobile responsiveness, I have a life.</p>
+      </div>
+
+      <div className="max-sm:hidden mx-5">
+
+      <Navbar/>
+      <div >
+        <Home/>
+        <Projects/>
+        <AboutMe/>
+        <Skills/>
+        <Contact/>
+      </div>
+      <Footer/>
       </div>
     </div>
   );
