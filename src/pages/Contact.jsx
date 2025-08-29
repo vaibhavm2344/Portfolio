536a56fbe9102b585059cd9ecfe136ebe68e8383
Vaibhav More <vaibhavcmore23@gmail.com>
2025-08-29 22:58:39 +0530

    fix form stylling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <section className='flex h-[100vh] pt-25' id='contact'>
       <div className='flex-1 flex justify-center items-center'>
-        <p className='text-xl text-center'>I'm always excited to connect with new people! Whether it's a project, a question, or just a hello, feel free to reach out.</p>
+        <p className='text-3xl font-semibold outfit text-center text-[var(--text-primary)]'>I'm always excited to connect with new people! Whether it's a project, a question, or just a hello, feel free to reach out.</p>
       </div>
       <div className='flex-1 flex-col'>
         <form action="" className='flex flex-col items-center border border-[var(--subheading-color)] mt-10 mx-20 py-8 px-10 bg-[var(--contact-form)] gap-5 rounded-t-2xl'>
